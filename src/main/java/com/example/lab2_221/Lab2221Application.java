e45be1c3cace635f6e9686594d92e897a080982a
Joaquin Orellana <joaquin.orellana@pucp.edu.pe>
2022-04-12 19:24:22 -0500

    Primer Commit

---
+++ src/main/java/com/example/lab2_221/Lab2221Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2221Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab2_221/Lab2221ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_221;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2221ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
